a379a57777acdf9ddaa2c82998a4d2b123bf948c
testOfCoder <1531990876@qq.com>
2015-08-26 23:33:56 +0800

    初始化项目

---
+++ test123/test123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test123
+//
+//  Created by wmg on 15/8/26.
+//  Copyright (c) 2015年 nlyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test123/test123/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test123
+//
+//  Created by wmg on 15/8/26.
+//  Copyright (c) 2015年 nlyx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
